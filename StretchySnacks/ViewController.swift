f925d376775e512e14e562453c84ccc68742e188
Cam McLeod <cammcleod3@gmail.com>
2019-07-11 11:50:57 -0700

    Initial Commit

---
+++ StretchySnacks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchySnacks
+//
+//  Created by Cameron Mcleod on 2019-07-11.
+//  Copyright © 2019 Cameron Mcleod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
